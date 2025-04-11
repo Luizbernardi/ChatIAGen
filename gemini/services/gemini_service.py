e1cb73242afb6bee31234f6987925cb2bea00895
LuizBernardi <luizbernardi37@gmail.com>
2025-04-11 20:03:57 -0300

    Estilizando

--- gemini/services/gemini_service.py
+++ gemini/services/gemini_service.py
@@ -1,9 +1,8 @@
-from google import genai
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyD0qoHqe60nmuoDbdd_jhkztILsBSpLqW8")
 
 def getResponse(prompt):
-    client = genai.Client(api_key="")
-
-    response = client.models.generate_content(
-        model="gemini-2.0-flash", contents=prompt
-    )
-    return(response.text)
+    model = genai.GenerativeModel('gemini-2.0-flash')  # modelo gratuito
+    response = model.generate_content(prompt)
+    return response.text
